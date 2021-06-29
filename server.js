5eb960d6b390550e85301e32181fb3ad81d67d3b
sabrinawhite4 <sabrinawhite4@me.com>
2021-06-29 12:30:28 -0600

    added apm

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const gradient = require('gradient-string');
 const path = require('path');
 const app = express();
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+    accessToken: '87499faa46e94da3b6f6cb889ad0dc06',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
 
 app.use(express.json());
 
 
 app.get('/', (req, res) => {
+    rollbar.log('Hello world!')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
